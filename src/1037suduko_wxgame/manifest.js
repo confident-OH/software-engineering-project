3c2d11df483d791d01e013df9deaee102de59a6a
confidentoh <confidentoh@foxmail.com>
2020-11-09 00:31:12 +0800

    init app

---
+++ src/1037suduko_wxgame/manifest.js
@@ -0,0 +1,8 @@
+require("./js/egret.js")
+require("./js/eui.js")
+require("./js/assetsmanager.js")
+require("./js/tween.js")
+require("./js/WXCloudApi.js")
+require("./js/egretmgobe.js")
+require("./js/main.js")
+require("./js/default.thm.js")
