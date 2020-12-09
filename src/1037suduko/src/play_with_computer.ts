b1772bfaa906edef8af6b190a7d065b997a0f5cf
Coccino <lfzhangzhan@163.com>
2020-12-09 14:26:14 +0800

    init default.thm.js is not defined

---
+++ src/1037suduko/src/play_with_computer.ts
@@ -0,0 +1,29 @@
+class play_with_computers extends eui.Component implements eui.UIComponent{
+    public new_man_b:eui.Button;
+    public randam_sb:eui.Button;
+    public soduko_chb:eui.Button;
+    public quit_to_main:eui.Button;
+    constructor(){
+        super();
+        this.skinName = "resource/eui_skins/myskin/play_with_computerSkin.exml"
+    }
+    protected partAdded(partName: string, instance: any):void{
+        super.partAdded(partName, instance);
+    } 
+    protected childrenCreated(): void{
+        super.childrenCreated();
+        //返回主界面
+        this.quit_to_main.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>{
+            SceneManager.removeScene(new Startscence());
+        }, this)
+        this.new_man_b.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>{
+            SceneManager.removeScene(new education());
+        }, this)
+        this.randam_sb.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>{
+            SceneManager.removeScene(new random());
+        }, this)
+        this.soduko_chb.addEventListener(egret.TouchEvent.TOUCH_TAP, ()=>{
+            SceneManager.removeScene(new challenges());
+        }, this)
+    }
+}
